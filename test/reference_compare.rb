23f49dc8d9970dec7a3e683dd824db9e8038ea9b
Earlopain <earlopain@protonmail.com>
2023-04-02 21:53:42 +0200

    Respect color permissions in reference comparison

--- test/reference_compare.rb
+++ test/reference_compare.rb
@@ -10,7 +10,8 @@ Zlib::GzipReader.open("dtext_reference.json.gz") do |file|
     puts i if i % 10_000 == 0
     json = JSON.parse(line)
 
-    dtext = DText.parse(json["i"], allow_color: false)[0]
+    allow_color = %w(wp ua ui).include?(json["id"][0..1])
+    dtext = DText.parse(json["i"], allow_color: allow_color)[0]
     if dtext != json["o"]
       differences << [json["id"], json["i"], json["o"], dtext]
     end
--- test/reference_generate.rb
+++ test/reference_generate.rb
@@ -8,7 +8,9 @@ Zlib::GzipWriter.open("dtext_reference.json.gz") do |output|
       puts i if i % 10_000 == 0
       json = JSON.parse(line)
 
-      dtext = DText.parse(json["body"], allow_color: false)[0]
+      allow_color = %w(wp ua ui).include?(json["id"][0..1])
+      dtext = DText.parse(json["body"], allow_color: allow_color)[0]
+
       output.puts({ id: json["id"], i: json["body"], o: dtext }.to_json + "\n")
     end
   end

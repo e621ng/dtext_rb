4cf73756ef8aef2c8e81216327aea957d49d35d5
Earlopain <earlopain@protonmail.com>
2023-04-02 14:21:30 +0200

    Fix unclosed tags inside headers
    https://github.com/danbooru/dtext_rb/commit/fcc2081127c6e798eb7695a8b01109592be1960c
    
    Co-Authored-By: evazion <noizave@gmail.com>

--- test/dtext_test.rb
+++ test/dtext_test.rb
@@ -197,6 +197,13 @@ test2[/ltable]
     assert_parse('<ul><li>one</li></ul><h1>header</h1>', "* one\nh1. header")
     assert_parse('<ul><li>one</li></ul><h1>header</h1><ul><li>two</li></ul>', "* one\nh1. header\n* two")
     assert_parse('<h1>header</h1><h2>header</h2>', "h1. header\nh2. header")
+
+    assert_parse('<h1><em>header</em></h1><p>blah</p>', "h1. [i]header\nblah")
+    assert_parse('<h1><span class="spoiler">header</span></h1><p>blah</p>', "h1. [spoiler]header\nblah")
+    assert_parse('<h1><a rel="nofollow" class="dtext-link" href="http://example.com">http://example.com</a></h1><p>blah</p>', %{h1. http://example.com\nblah})
+    assert_parse('<h1><a rel="nofollow" class="dtext-link dtext-external-link" href="http://example.com">example</a></h1><p>blah</p>', %{h1. "example":http://example.com\nblah})
+
+    assert_parse('<blockquote><blockquote><h1>header</h1></blockquote></blockquote>', %{[quote]\n\n[quote]\n\nh1. header\n[/quote]\n\n[/quote]})
   end
 
   def test_quote_blocks
